9808a8deaf935f43a8ca508979ab9f56c395b73c
Tomas M <tjmonteiro@mail.austral.edu.ar>
2025-10-15 17:19:38 -0300

    feat: integrate Auth0 for authentication and update application configuration

--- auth/src/main/kotlin/auth/AuthenticationServiceApplication.kt
+++ auth/src/main/kotlin/auth/AuthenticationServiceApplication.kt
@@ -1,8 +1,7 @@
-package org.austral.ingsis.auth
+package auth
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,39 +10,32 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
 @SpringBootApplication
 class AuthenticationServiceApplication {
 
     @GetMapping("/")
-    fun index(): String {
-        return "I'm Alive!"
-    }
+    fun index(): String = "I'm Alive!"
 
     @GetMapping("/jwt")
-    fun jwt(@AuthenticationPrincipal jwt: Jwt): String {
-        return jwt.tokenValue
-    }
+    fun jwt(
+        @AuthenticationPrincipal jwt: Jwt,
+    ): String = jwt.tokenValue
 
     @GetMapping("/snippets")
-    fun getAllMessages(): String {
-        return "secret message"
-    }
+    fun getAllMessages(): String = "secret message"
 
     @GetMapping("/snippets/{id}")
-    fun getSingleMessage(@PathVariable id: String): String {
-        return "secret message $id"
-    }
+    fun getSingleMessage(
+        @PathVariable id: String,
+    ): String = "secret message $id"
 
     @PostMapping("/snippets")
-    fun createMessage(@RequestBody message: String?): String {
-        return String.format("Message was created. Content: %s", message)
-    }
+    fun createMessage(
+        @RequestBody message: String?,
+    ): String = String.format("Message was created. Content: %s", message)
 }
 
 fun main(args: Array<String>) {
-	runApplication<AuthenticationServiceApplication>(*args)
+    runApplication<AuthenticationServiceApplication>(*args)
 }
-
-
--- auth/src/test/kotlin/auth/AuthenticationServiceApplicationTests.kt
+++ auth/src/test/kotlin/auth/AuthenticationServiceApplicationTests.kt
@@ -1,4 +1,4 @@
-package org.austral.ingsis.auth
+package auth
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class AuthenticationServiceApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
