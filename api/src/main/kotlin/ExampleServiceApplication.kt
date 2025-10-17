234a7b85d95157007f96a75a484e8fb345307639
Tomas M <tjmonteiro@mail.austral.edu.ar>
2025-10-17 08:34:41 -0300

    feat: update build configuration and modify application scanning for security package

--- api/src/main/kotlin/ExampleServiceApplication.kt
+++ api/src/main/kotlin/ExampleServiceApplication.kt
@@ -3,7 +3,7 @@ package api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = ["api"])
+@SpringBootApplication(scanBasePackages = ["api", "auth.security"])
 class ExampleServiceApplication
 
 fun main(args: Array<String>) {
